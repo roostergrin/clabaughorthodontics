40a66bfe54da27cfb62fc405df9153329e91621a
Matt Sprague <me@spragala.com>
2020-01-17 11:49:14 -0800

    Initial commit

---
+++ src/components/base/base-lazy-img/base-lazy-img.vue
@@ -0,0 +1,54 @@
+<template lang='pug' src='./base-lazy-img.pug'></template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String
+    },
+    bgColor: {
+      type: String,
+      default: '#ffffff'
+    },
+    imageBackground: {
+      type: Boolean,
+      default: false
+    },
+    alt: {
+      type: String,
+      default: document.location.hostname + ' image for section'
+    }
+  },
+  data () {
+    return {
+      currentImg: null,
+      imageType: 'relative',
+      loading: true,
+      loaded: false,
+      intersectionOptions: {
+        root: null,
+        rootMargin: '500px 0px 0px 0px',
+        threshold: [0.01]
+      }
+    }
+  },
+  created () {
+    if (this.imageBackground) this.imageType = 'absolute'
+  },
+  mounted () {
+    this.loaded = true
+  },
+  methods: {
+    onWaypoint ({ going, direction }) {
+      if (going === 'in') {
+        let downloadingImage = new Image()
+        this.currentImg = this.src
+        downloadingImage.onload = () => {
+          this.loading = false
+        }
+        downloadingImage.src = this.src
+      }
+    }
+  }
+}
+</script>
