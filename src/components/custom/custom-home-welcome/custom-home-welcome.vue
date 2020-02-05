f0d262eee35161dd332168b88c3b68b1e995c5f8
Matt Sprague <me@spragala.com>
2020-02-05 11:57:56 -0800

    feat(welcome): creates welcome section

---
+++ src/components/custom/custom-home-welcome/custom-home-welcome.vue
@@ -0,0 +1,26 @@
+<template lang="pug" src="./custom-home-welcome.pug" ></template>
+
+<script>
+export default {
+  props: {
+    props: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data: () => ({
+    videoPlaying: false
+  }),
+  methods: {
+    playVideo () {
+      this.videoPlaying = true
+      setTimeout(() => {
+        this.$refs.video.play()
+      }, 250)
+    },
+    moveDown () {
+      this.$store.dispatch('ACTIVE_INDEX', this.$store.state.currIndex + 1)
+    }
+  }
+}
+</script>
