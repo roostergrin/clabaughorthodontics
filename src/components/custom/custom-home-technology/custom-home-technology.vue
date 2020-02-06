326809f0cc36893a79716427f3f4e566974150fe
Matt Sprague <me@spragala.com>
2020-02-06 13:56:29 -0800

    feat(technology): creates technology section and adds to home page

--- src/components/custom/custom-home-technology/custom-home-technology.vue
+++ src/components/custom/custom-home-technology/custom-home-technology.vue
@@ -7,6 +7,53 @@ export default {
       type: Object,
       default: () => ({})
     }
+  },
+  data: () => ({
+    currentSlide: 0,
+    sliding: false,
+    maxHeight: null
+    // animate: null
+  }),
+  mounted () {
+    const setHeight = setInterval(() => {
+      if (this.$refs !== undefined) {
+        this.getHeight()
+        clearInterval(setHeight)
+      }
+    }, 100)
+    window.addEventListener('resize', () => {
+      setTimeout(() => {
+        this.windowWidth = window.innerWidth
+        this.getHeight()
+      }, 500)
+    })
+    // this.animate = setInterval(() => {
+    //   this.changeSlide('auto')
+    // }, 5000)
+  },
+  methods: {
+    getHeight () {
+      const newArr = []
+      this.$refs.content.map((el) => {
+        newArr.push(el.clientHeight)
+      })
+      this.maxHeight = Math.max(...newArr)
+    },
+    changeSlide (str) {
+      if (str === 'prev' && !this.sliding) {
+        this.currentSlide === 0 ? this.currentSlide = this.props.list.length - 1 : this.currentSlide--
+      }
+      if (str === 'next' && !this.sliding) {
+        this.currentSlide === (this.props.list.length - 1) ? this.currentSlide = 0 : this.currentSlide++
+      }
+      this.sliding = true
+      setTimeout(() => {
+        this.sliding = false
+      }, 1000)
+    },
+    moveDown () {
+      this.$store.dispatch('ACTIVE_INDEX', this.$store.state.currIndex + 1)
+    }
   }
 }
 </script>
