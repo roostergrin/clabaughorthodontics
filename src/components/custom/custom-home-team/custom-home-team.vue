0c45eec58ff6b15574536d201680fdccf7ee05ed
Matt Sprague <me@spragala.com>
2020-02-10 14:56:14 -0800

    updates(team): creates team component | WIP mobile

--- src/components/custom/custom-home-team/custom-home-team.vue
+++ src/components/custom/custom-home-team/custom-home-team.vue
@@ -7,6 +7,32 @@ export default {
       type: Object,
       default: () => ({})
     }
+  },
+  data: () => ({
+    currIndex: 4,
+    maxHeight: '',
+    windowWidth: window.innerWidth
+  }),
+  mounted () {
+    this.getHeight()
+    window.addEventListener('resize', () => {
+      setTimeout(() => {
+        this.windowWidth = window.innerWidth
+        this.getHeight()
+      }, 500)
+    })
+  },
+  methods: {
+    getHeight () {
+      const newArr = []
+      this.$refs.bio.map((el) => {
+        newArr.push(el.clientHeight)
+      })
+      this.maxHeight = Math.max(...newArr)
+    },
+    handleHover (i) {
+      this.currIndex = i
+    }
   }
 }
 </script>
