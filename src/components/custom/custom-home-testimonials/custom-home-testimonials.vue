5270d9d895fc43cf272f175ffa5f7e84dc2b1553
Matt Sprague <me@spragala.com>
2020-02-11 09:56:32 -0800

    feat(testimonials): creates testimonial section | TODO: mobile styles

--- src/components/custom/custom-home-testimonials/custom-home-testimonials.vue
+++ src/components/custom/custom-home-testimonials/custom-home-testimonials.vue
@@ -7,6 +7,40 @@ export default {
       type: Object,
       default: () => ({})
     }
+  },
+  data: () => ({
+    currIndex: 0,
+    maxHeight: 0,
+    windowWidth: window.innerWidth
+  }),
+  mounted () {
+    this.getHeight()
+    window.addEventListener('resize', () => {
+      setTimeout(() => {
+        this.windowWidth = window.innerWidth
+        this.getHeight()
+      }, 250)
+    })
+  },
+  methods: {
+    getHeight () {
+      const newArr = []
+      this.$refs.testimonial.map((el) => {
+        newArr.push(el.clientHeight)
+      })
+      this.maxHeight = Math.max(...newArr)
+    },
+    changeSlide (str) {
+      if (str === 'prev') {
+        this.currIndex === 0 ? this.currIndex = this.props.testimonials.length - 1 : this.currIndex--
+      }
+      if (str === 'next') {
+        this.currIndex === (this.props.testimonials.length - 1) ? this.currIndex = 0 : this.currIndex++
+      }
+    },
+    moveDown () {
+      this.$store.dispatch('ACTIVE_INDEX', this.$store.state.currIndex + 1)
+    }
   }
 }
 </script>
