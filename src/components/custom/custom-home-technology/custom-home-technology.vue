3576b5c90daa6f58385ae04a6cbd9f5ddd24391c
Matt Sprague <me@spragala.com>
2020-02-28 07:44:10 -0800

    updates(touch): adjusts touch sensitivity

--- src/components/custom/custom-home-technology/custom-home-technology.vue
+++ src/components/custom/custom-home-technology/custom-home-technology.vue
@@ -66,7 +66,7 @@ export default {
       var startY
       var distX
       var distY
-      var threshold = 5 // min dist for swipe
+      var threshold = 50 // min dist for swipe
       var restraint = 50 // max dist allow allowed at the same time in perpendicular direction
       var allowedTime = 300 // max time allowed to travel dist
       var elapsedTime
